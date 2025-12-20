0645e91e129d685bc00fb2a7ae3796e08b29de9c
matthias122 <ahrensmatthias122@gmail.com>
2025-12-20 06:55:18 +0100

    add generated files

---
+++ lib/l10n/app_localizations_de.dart
@@ -0,0 +1,22 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for German (`de`).
+class AppLocalizationsDe extends AppLocalizations {
+  AppLocalizationsDe([String locale = 'de']) : super(locale);
+
+  @override
+  String get welcome => 'Welcome';
+
+  @override
+  String get email => 'Email';
+
+  @override
+  String get password => 'Password';
+
+  @override
+  String get login => 'Login';
+}
